a6b017d17aee9e4fb7aa5eb2544be2d0d3b8a138
chris-wamae <chriswamae123@gmail.com>
2023-03-24 12:42:18 +0300

    Added application routes to routes.rb

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,3 @@
 class HerosController < ApplicationController
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :hero_powers
-  resources :powers
-  resources :heros
+  resources :hero_powers, only:[:create]
+  resources :powers, only:[:index,:create,:show]
+  get "heroes", to: "heros#create"
+  get "heroes/:id", to: "heros#show"
+  post "heroes", to: "heros#post"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
