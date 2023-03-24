b9acd4b8be59458a773bebed32914a3c23ba2010
chris-wamae <chriswamae123@gmail.com>
2023-03-24 16:14:45 +0300

    Added controller methods to HeroPowers controller

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,16 @@
 class HeroPowersController < ApplicationController
+
+    def create
+    new_hero_power = HeroPower.create(allow_params)
+    if new_hero_power.valid?
+    render json: new_hero_power.hero,except:[:created_at,:updated_at], include:[powers:{except:[:created_at,:updated_at]}]
+    else
+        render json: {errors:["Validation errors"]}
+    end
+    end
+
+    private
+    def allow_params
+    params.permit(:strength,:power_id,:hero_id)
+    end
 end
--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -2,5 +2,7 @@ class HeroPower < ApplicationRecord
     belongs_to :power
     belongs_to :hero
     validates :strength,presence:true
-    validates :strength,inclusion: { in: %w(small medium large)}
+    validates :strength,inclusion: { in: %w(Strong Weak Average)}
+    validates :power_id,presence:true
+    validates :hero_id,presence:true
 end
