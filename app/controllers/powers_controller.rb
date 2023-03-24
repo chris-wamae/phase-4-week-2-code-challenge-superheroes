c9bfd3ae8e1de790db79bc683f79933e883a79f8
chris-wamae <chriswamae123@gmail.com>
2023-03-24 15:59:36 +0300

    Added controller methods for powers routes

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,41 @@
 class PowersController < ApplicationController
 
+    def index
+    powers = Power.all
+    render json: powers,except:[:created_at,:updated_at]
+    end
 
+    def show
+        begin
+        power = power_by_id
+        render json: power,except:[:created_at,:updated_at]
+        rescue ActiveRecord::RecordNotFound
+        render json:{error:"Power not found"},status: :not_found
+        end
+    end
+
+    def update
+        begin
+        power = power_by_id
+        power.update(allow_params)
+        if power.valid?
+        render json: power, except:[:created_at,:updated_at],status:200
+        else
+        render json: {errors:["Validation errors"]},status:403
+        end
+        rescue ActiveRecord::RecordNotFound
+        render json:{error:"Power not found"},status: :not_found
+        end
+    end
+    
+    private
+    
+    def power_by_id
+    Power.find(params[:id])
+    end
+
+    def allow_params
+        params.permit(:description)
+    end
     
 end
