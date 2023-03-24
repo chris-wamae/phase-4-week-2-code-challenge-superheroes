ace7510e332866c8f79aab2652449ff485006b17
chris-wamae <chriswamae123@gmail.com>
2023-03-24 15:04:35 +0300

    Added validations for strength and description columns

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,6 @@
 class HeroPower < ApplicationRecord
     belongs_to :power
     belongs_to :hero
+    validates :strength,presence:true
+    validates :strength,inclusion: { in: %w(small medium large)}
 end
