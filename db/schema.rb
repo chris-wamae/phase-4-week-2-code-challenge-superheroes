e23b8ce3a7a477ac7ea19e52339e4b5e433e906b
chris-wamae <chriswamae123@gmail.com>
2023-03-26 19:40:31 +0300

    Changed database to sqlite3 from postgress

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_24_090354) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "hero_powers", force: :cascade do |t|
     t.string "strength"
     t.integer "hero_id"
